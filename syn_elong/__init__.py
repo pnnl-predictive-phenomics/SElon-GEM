4b8df7d53df19abd5efa2e311339660d99d7fc6c
James <james.ch.pino@gmail.com>
2023-03-30 11:42:58 -0400

    Renaming excreted metabolites folder structure.

--- syn_elong/__init__.py
+++ syn_elong/__init__.py
@@ -11,6 +11,6 @@ model = cobra.io.read_sbml_model(
 
 exp_file_path = path.joinpath('data', 'experiments.yml').__str__()
 expected_metab = pd.read_csv(
-    path.joinpath('data', 'excreted_syn.csv').__str__(),
+    path.joinpath('data', 'excreted', 'excreted_syn.csv').__str__(),
     index_col=0,
 ).bigg_id
