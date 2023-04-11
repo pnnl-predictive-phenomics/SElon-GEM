f0a72d06cb86f76218c3cb8f1a3bd00172c75ba6
James <james.ch.pino@gmail.com>
2023-04-11 12:08:13 -0400

    Removing index column from files.

--- syn_elong/__init__.py
+++ syn_elong/__init__.py
@@ -12,5 +12,4 @@ model = cobra.io.read_sbml_model(
 exp_file_path = path.joinpath('data', 'experiments.yml').__str__()
 expected_metab = pd.read_csv(
     path.joinpath('data', 'excreted', 'metabolites.csv').__str__(),
-    index_col=0,
 ).bigg_id
