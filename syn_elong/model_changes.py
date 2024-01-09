686d74cf15d09db44cd292304325fdc4e0272ee1
James <james.ch.pino@gmail.com>
2024-01-09 17:09:50 -0500

    Adding reactions from ijb792, removing reactions added from gapfill. Keeping those separate until additional evidence is found to support addition.

--- syn_elong/model_changes.py
+++ syn_elong/model_changes.py
@@ -23,9 +23,7 @@ starting_model_f_name = 'iJB785.xml'
 s_model_path = os.path.join(_file_path, starting_model_f_name)
 
 starting_model = cobra.io.read_sbml_model(s_model_path)
-# starting_model = cobra.io.load_json_model('iMS837.json')
-# cobra.io.write_sbml_model(starting_model, 'iMS837.xml')
-# starting_model = cobra.io.read_sbml_model('iMS837.xml')
+# starting_model = cobra.io.load_json_model('iJB792.json')
 starting_model.id = "syn_elong"
 
 output_model_name = 'syn_elong.xml'
@@ -49,14 +47,11 @@ def update_2(model):
 
     # Copy sucrose transport from universal model
     sucr_transport = universal_model.reactions.SUCRt2.copy()
-
-    # Set the lower bounds for the sucrose reactions
-    sucr_transport.lower_bound = -1000.0
-    model.reactions.EX_sucr_e.lower_bound = 0.011
-
+    sucr_transport.lower_bound = -1000
     # Add the transport reaction to the model
     model.add_reactions([sucr_transport])
-
+    # Set the lower bounds for the sucrose reactions
+    model.reactions.EX_sucr_e.lower_bound = 0.011
     # Add the gene name of the sucrose transporter to the model
     gene_add = cobra.core.Gene(id='cscB', name='cscB', functional=True)
     model.genes.add(gene_add)
@@ -66,10 +61,40 @@ def update_2(model):
     return model
 
 
+def update_3(model):
+    # updates the model to add the reactions from ijb792
+    rxns = ['MPTSS', 'MOADSUx', 'GTPC', 'CPMPS', 'MPTS', 'MPTAT', 'MOCOS', 'MDH']
+    reactions_to_add = set()
+    for reaction_id in rxns:
+        reaction = universal_model.reactions.get_by_id(reaction_id)
+        reactions_to_add.add(reaction.copy())
+    model.add_reactions(reactions_to_add)
+    model.remove_reactions([universal_model.reactions.get_by_id('ORNTA')])
+    return model
+
+
+def update_4(model):
+    # adding reactions from gapfilling
+    reaction_ids = {
+        'GLCS1', 'SADT2', 'ATPSu', 'PPND2', 'aratyr1', 'CDPDAGS1819Z160',
+        'ASNTRS_1', 'GLGB', 'FRDO', 'ACGAM6PS', 'PGPS1819Z160', 'PAPSSH', 'GLYOX_2',
+        'APSR', 'AIRC1', 'ILETA2', 'PRFGS_1', 'AHSERL2', 'FASC161ACP', 'GLCS2', 'UAGDP_c', 'P5CCD', 'UGP_PGM_c',
+    }
+    reactions_to_add = set()
+    for reaction_id in reaction_ids:
+        reaction = universal_model.reactions.get_by_id(reaction_id)
+        reactions_to_add.add(reaction.copy())
+    model.add_reactions(reactions_to_add)
+    return model
+
+
 def process_model_steps():
     # Fix compartments
     model = update_1(starting_model)
     model = update_2(model)
+    model = update_3(model)
+    # don't add these reactions yet
+    # model = update_4(model)
     write_model(model)
 
 
@@ -80,5 +105,5 @@ if __name__ == '__main__':
         [
             *model_paths,
             '--filename', os.path.join(_file_path, 'model_differences.html')
-         ]
+        ]
     )
