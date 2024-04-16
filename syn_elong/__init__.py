27f485588abfbc7f59c30fdb3afd538fb2db0c44
James <james.ch.pino@gmail.com>
2024-04-16 15:16:36 -0400

    Revert "Updating repo to match repo on comp-bio. Adding license and disclaimer."
    This reverts commit 5fa647354f1a0ebc31c8dc0fc7d511690351edd6.

--- syn_elong/__init__.py
+++ syn_elong/__init__.py
@@ -8,8 +8,6 @@ path = pathlib.Path(__file__).parent
 model = cobra.io.read_sbml_model(
     path.joinpath('syn_elong.xml').__str__()
 )
-ijb792 = cobra.io.load_json_model(path.joinpath('iJB792.json').__str__())
-ims837 =cobra.io.load_json_model(path.joinpath('iMS837.json').__str__())
 
 exp_file_path = path.joinpath('data', 'experiments.yml').__str__()
 expected_metab = pd.read_csv(
